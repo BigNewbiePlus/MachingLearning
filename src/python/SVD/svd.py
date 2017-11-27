3c4a5d633298c63591fa94f5c18646f047388944
fdh <dhfang812@163.com>
2017-11-27 18:46:12 +0800

    SVD编写完成

--- src/python/SVD/svd.py
+++ src/python/SVD/svd.py
@@ -34,16 +34,18 @@ def loadExData3():
 
 
 # 基于欧氏距离求解相似度
-def ecludSim(inA, inB):
+def euclidSim(inA, inB):
     return 1.0/(1.0 + la.norm(inA-inB))
 
 # 基于皮尔逊相关系数计算相似度
 def pearsSim(inA, inB):
+    if len(inA)<3:
+        return 1.0
     return 0.5 + 0.5 * np.corrcoef(inA, inB, rowvar=0)[0][1]
 
 # 基于余弦值计算相似度
 def cosSim(inA, inB):
-    cos = float(inA.T*intB)/(la.norm(inA)*la.norm(inB))
+    cos = float(inA.T*inB)/(la.norm(inA)*la.norm(inB))
     return 0.5 + 0.5*cos
 
 # 基于非SVD计算两物品相似度给未打分item打分
@@ -52,9 +54,9 @@ def standEst(dataMat, user, simMeas, item):
 
     Args:
         dataMat 用户评分数据集。行表示用户，列表示商品菜单
-        user 要估算商品的用户，用行号表示
+        user 要估算商品的用户，为数据矩阵dataMat的行号
         simMeas 相似度计算方法，有欧式、皮尔逊系数、余弦相似度三种
-        item 用户user未评价商品号，用列号表示
+        item 用户user未评价商品号，为数据矩阵dataMat的列号
 
     Return:
         ratSimTotal/simTotal 评分(0~5之间)
@@ -94,8 +96,8 @@ def standEst(dataMat, user, simMeas, item):
 
 # 基于SVD计算两物品相似度给未打分item打分
 def svdEst(dataMat, user, simMeas, item):
-    """svdEst(使用基于SVD降维方法计算两物品相似度。和传统standEst相比，将MxN矩阵分解为u(MxM),sig(MxN),vT(NxN)三个矩阵,
-              将数据降到k维变为u(M*k) * np.diag(s)(k*k) * vT(k*N))
+    """svdEst(使用基于SVD降维方法计算两物品相似度降低计算复杂度。和传统standEst相比，将MxN数据矩阵dataMat分解为u(MxM),sig(MxN),vT(NxN)三个矩阵,
+              将数据降到k维变为u(M*k) * np.diag(s)(k*k) * vT(k*N), 其中v(N*k))
 
     Args:
         同standEst
@@ -104,19 +106,19 @@ def svdEst(dataMat, user, simMeas, item):
     """
     n = np.shape(dataMat)[1]
     ratSimTotal = 0.0
-    simTottal = 0.0
+    simTotal = 0.0
 
     # 计算svd
     k = 4
-    u, sigma, v = la.svd(dataMat)
-    sigK = np.eye(k)*sigma[:k]
-    transformItemsV = dataMat.T * u[:,:k] * sigk.I
+    u, sigma, vT = la.svd(dataMat)
+    Sigk = np.mat(np.eye(k)*sigma[:k])
+    transformItemsV = dataMat.T * u[:,:k] * Sigk.I
 
-    print("dataMat", np.shape(dataMat))
-    print("u", np.shape(u[:,k]))
-    print("sigk", np.shape(sigk))
-    print("v", np.shape(v[:k,:]))
-    print("transformItemsV", np.shape(transformItemsV))
+    #print("dataMat", np.shape(dataMat))
+    #print("u", np.shape(u[:,:k]))
+    #print("Sigk", np.shape(Sigk))
+    #print("vT", np.shape(vT[:k,:]))
+    #print("transformItemsV", np.shape(transformItemsV))
 
     for i in range(n):
         userRating = dataMat[user, i]
@@ -125,33 +127,33 @@ def svdEst(dataMat, user, simMeas, item):
 
         # 基于SVD降维的数据求解相似度
         similarity = simMeas(transformItemsV[item, :].T, transformItemsV[i, :].T)
-
+        print('The %d and %d similarity is %f'%(item, i, similarity))
         # 求解总相似度和总评分
         simTotal += similarity
         ratSimTotal += userRating * similarity
 
-    if simTotal ==0:
+    if simTotal == 0:
         return 0
     else:
         return ratSimTotal/simTotal
 
 # 推荐系统
-def recommand(dataMat, user, topNItems=3, simMeas=cosSim, estMethod=standEst):
-    """recommand(给用户推荐前topNItem未打分商品，默认为3)
+def recommand(dataMat, user, topNItems=3, simMeas=euclidSim, estMethod=svdEst):
+    """recommand(给用户推荐前topNItem种未打分商品，默认为3)
 
     Args:
         dataMat 用户数据
         user 用户
         topNItems 打分靠前的N商品
-        simMeas 相似度估计方法，默认使用cosSim
-        estMethod 打分策略,默认使用标准估计方法
+        simMeas 相似度估计方法，默认使用euclidSim
+        estMethod 打分策略,默认使用svd降维估计方法
 
     Returns:
         返回N个推荐商品
 
     """
     unRatedItems = np.nonzero(dataMat[user,:]==0)[1]
-    if len(unRatedItems):
+    if len(unRatedItems)==0:
         return "You rated everything"
 
     itemScores = []
@@ -159,7 +161,7 @@ def recommand(dataMat, user, topNItems=3, simMeas=cosSim, estMethod=standEst):
         rateScore = estMethod(dataMat, user, simMeas, item)
         itemScores.append((item, rateScore))
 
-    return sorted(itemScore, key=lambda x:x[1], reversed=True)[:topNItems]
+    return sorted(itemScores, key=lambda x:x[1], reverse=True)[:topNItems]
 
 def analyse_data(Sigma, Num=20):
     """analyse_data(分析Sigma的取值个数，默认为20)
@@ -171,11 +173,11 @@ def analyse_data(Sigma, Num=20):
 
     Sigma2 = Sigma**2
     SigmaSum = float(np.sum(Sigma2))
-    sigmaTotal = 0.0
-    Num = min(len(Sigma2), NUM)
+    SigmaTotal = 0.0
+    Num = min(len(Sigma2), Num)
     for i in range(Num):
-        sigmaTotal += Sigma2[i]
-        print("主成分:%d, 方差占比:%.2f"%(i+1, sigmaTotal*100/sigmaSum))
+        SigmaTotal += Sigma2[i]
+        print("主成分:%d, 方差占比:%.2f"%(i+1, SigmaTotal*100/SigmaSum))
 
 # 图像压缩
 
@@ -183,55 +185,58 @@ def analyse_data(Sigma, Num=20):
 def loadImgData(filename):
     return np.mat([[int(line[i]) for i in range(32)] for line in open(filename).readlines()])
 
-# 打印图像
-def printImgData(imgMat, threshold):
+# 打印图像矩阵
+def printImgMat(imgDataMat, threshold):
     for i in range(32):
+        row = ""
         for j in range(32):
-            if float(imgMat[i,j])>threshold:
-                print 1,
-            else
-                print 0,
-            print ''
+            if float(imgDataMat[i,j])>threshold:
+                row+='1'
+            else:
+                row+='0'
+        print(row)
 
 # 图像压缩重构
-def imgCompress(numSVD=3, thresh=0.8):
-    """ImgCompress(图像压缩，借助SVD将图像压缩后重新构建)
+def imgCompress(imgDataMat, numSVD=3, thresh=0.8):
+    """ImgCompress(图像压缩重构，借助SVD将图像分解为u,s,vT三部分后降维(n->k)，然后重建)
 
     Args:
-        numSVD 降维后维度
-        trhresh 图像阈值
+        imgDataMat 图像矩阵(0数字)
+        numSVD 降维维度
+        thresh 图像阈值
     """
-    imgMat = loadImagData('../../../data/SVD/0.txt')
-    printImgMat(imgMat, thresh)
+    printImgMat(imgDataMat, thresh)
 
-    u,Sigma,v = la.svd(imgMat)
-
-    analysis_data(Sigma, 20)
-
+    u,Sigma,vT = la.svd(imgDataMat)
+    # 分析各维度方差占比
+    analyse_data(Sigma, 20)
+    # 对图像降维，然后重构图像
     SigReco = np.eye(numSVD)*Sigma[:numSVD]
-    reconMat = u[:, :numSVD] * SigReco * v[:numSVD,:]
-    printImgMat(reconMat, thresh)
+    recoMat = u[:, :numSVD] * SigReco * vT[:numSVD,:]
+    printImgMat(recoMat, thresh)
 
 def main():
 
     dataMat = loadExData3()
-    recItems1 = recommand(dataMat, user=0, topNItems=3, simMeas=cosSim, estMethod=standEst)
-    recItems2 = recommand(dataMat, user=0, topNItems=3, simMeas=EucldSim, estMethod=standEst)
-    recItems3 = recommand(dataMat, user=0, topNItems=3, simMeas=PersSim, estMethod=standEst)
+    dataMat = np.mat(dataMat)
+    print("dataMat:")
+    print(dataMat)
+    #recItems = recommand(dataMat, user=1, topNItems=3, simMeas=cosSim, estMethod=standEst)
+    #print(recItems)
+    recItems = recommand(dataMat, user=1, topNItems=3, simMeas=cosSim, estMethod=svdEst)
+    print(recItems)
+    #recItems = recommand(dataMat, user=1, topNItems=3, simMeas=euclidSim, estMethod=standEst)
+    #recItems = recommand(dataMat, user=1, topNItems=3, simMeas=euclidSim, estMethod=svdEst)
 
-    recItems4 = recommand(dataMat, user=1, topNItems=3, simMeas=cosSim, estMethod=svdEst)
-    recItems5 = recommand(dataMat, user=1, topNItems=3, simMeas=EucldSim, estMethod=svdEst)
-    recItems6 = recommand(dataMat, user=1, topNItems=3, simMeas=PersinSim, estMethod=svdEst)
+    #recItems = recommand(dataMat, user=1, topNItems=3, simMeas=pearsSim, estMethod=standEst)
+    recItems = recommand(dataMat, user=1, topNItems=3, simMeas=pearsSim, estMethod=svdEst)
+    print(recItems)
+    imgDataMat = loadImgData("../../../data/SVD/0_5.txt")
 
-    print(recItem1)
-    print(recItem2)
-    print(recItem3)
-    print(recItem4)
-    print(recItem5)
-    print(recItem6)
+    imgCompress(imgDataMat, numSVD=3, thresh=0.8)
 
-    imgCompress(numSVD=3， thresh=0.8)
-
+if __name__ == '__main__':
+    main()
 
 
 
